fb23b1ddfa1d942efe00f36e27892300a0d20d9e
Arpit <arpitkumar15583@gmail.com>
2024-12-30 05:04:55 +0530

    metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,23 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
+// Local fonts with proper configurations
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+// Metadata configuration
 export const metadata: Metadata = {
   title: "Madhurima-The Music Society of DTU",
-  description: "A Website alla bout DTU's home of eccentric musicians",
+  description: "A Website all about DTU's home of eccentric musicians",
 };
 
 export default function RootLayout({
@@ -28,13 +31,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Favicon */}
         <link
           rel="icon"
           href="/img/Madhurima-Favicon.png"
           type="image/png"
         />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+
+        {/* Page Title */}
+        <title>{String(metadata.title)}</title>
+
+
+        {/* Meta Description */}
+        <meta name="description" content={metadata.description || ""} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -46,3 +55,4 @@ export default function RootLayout({
     </html>
   );
 }
+
